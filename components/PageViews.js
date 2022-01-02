041725524b087111e7ea76deb67fa7f38ae268c8
Snopkowski <wojciechsnopkowski@gmail.com>
2022-01-02 23:28:09 +0000

    blog page spinner

--- components/PageViews.js
+++ components/PageViews.js
@@ -1,10 +1,11 @@
 import useSWR from 'swr';
 import fetcher from 'lib/fetcher';
+import { Spinner } from '@chakra-ui/react';
 
 const PageViews = ({ slug }) => {
   const { data } = useSWR(`/api/views/${slug}`, fetcher);
 
-  return <>{data?.total ? `${data.total} views` : `–––`}</>;
+  return <>{data?.total ? `${data.total} views` : <Spinner size='xs' />}</>;
 };
 
 export default PageViews;
